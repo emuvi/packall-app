1af5e9ff5d493f6350e18ab96c0680e40f4faea0
Éverton M. Vieira <everton.muvi@gmail.com>
2021-12-09 20:26:39 -0300

    prototype development

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,50 @@
 import {
-    QinColumn, QinField, QinLine, QinInteger, QinPath, QinBoolean, QinString
+    QinColumn, QinField, QinLine, QinInteger, QinPath, QinBoolean, QinTabs
 } from "qinpel-cps"
 import { QinFilesNature } from "qinpel-res";
 
 class PackAll extends QinColumn {
 
     private qinLine: QinLine = new QinLine();
-    private qinBody: QinField = new QinField("Body", new QinPath(QinFilesNature.DIRECTORIES));
+    private qinBody: QinField = new QinField("Body", new QinPath({
+        nature: QinFilesNature.DIRECTORIES
+    }));
     private qinSpeed: QinField = new QinField("Speed", new QinInteger());
     private qinClean: QinField = new QinField("Clean", new QinBoolean());
     private qinFeed: QinField = new QinField("Feed", new QinPath());
+
+    private lineTabs = new QinLine();
+    private qinTabs = new QinTabs({
+        initial: [
+            {
+                title: "Tab 1", viewer: new QinColumn({
+                    initial: [
+                        new QinField("Speed1", new QinInteger()),
+                        new QinField("Clean1", new QinBoolean()),
+                        new QinField("Feed1", new QinPath()),
+                    ]
+                })
+            },
+            {
+                title: "Tab 2", viewer: new QinColumn({
+                    initial: [
+                        new QinField("Speed2", new QinInteger()),
+                        new QinField("Clean2", new QinBoolean()),
+                        new QinField("Feed2", new QinPath()),
+                    ]
+                })
+            },
+            {
+                title: "Tab 3", viewer: new QinColumn({
+                    initial: [
+                        new QinField("Speed3", new QinInteger()),
+                        new QinField("Clean3", new QinBoolean()),
+                        new QinField("Feed3", new QinPath()),
+                    ]
+                })
+            },
+        ]
+    });
 
     public constructor() {
         super();
@@ -18,6 +53,8 @@ class PackAll extends QinColumn {
         this.qinSpeed.install(this.qinLine);
         this.qinClean.install(this.qinLine);
         this.qinFeed.install(this.qinLine);
+        this.lineTabs.install(this);
+        this.qinTabs.install(this.lineTabs);
     }
 
 }

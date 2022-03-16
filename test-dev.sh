4f1ff2ae2edd7d880e6dfbcfbec945aa693ea4bf
Éverton M. Vieira <everton.muvi@gmail.com>
2022-03-16 11:55:31 -0300

    prototype development

--- test-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-bash -v build.sh
-browserify build/index.js --debug -o public/index.js
-rm -rf ~/Devs/run/app/packall-app
-mkdir ~/Devs/run/app/packall-app
-cd public
-cp -r * ~/Devs/run/app/packall-app
-cd ..
--- test-dsk.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-bash -v build.sh
-browserify build/index.js --debug -o public/index.js
-rm -rf ../qinpel-dsk/run/app/packall-app
-mkdir ../qinpel-dsk/run/app/packall-app
-cd public
-cp -r * ../../qinpel-dsk/run/app/packall-app
-cd ..
